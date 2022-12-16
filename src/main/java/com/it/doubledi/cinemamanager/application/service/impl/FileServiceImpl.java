2bbb5c0843b9f9c7819485ff66d3327775b93f1a
dong.dm <dong.dm@evotek.vn>
2022-12-16 17:26:22 +0700

    change store file to firebase

--- src/main/java/com/it/doubledi/cinemamanager/application/service/impl/FileServiceImpl.java
+++ src/main/java/com/it/doubledi/cinemamanager/application/service/impl/FileServiceImpl.java
@@ -1,37 +1,49 @@
 package com.it.doubledi.cinemamanager.application.service.impl;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.it.doubledi.cinemamanager.application.service.FileService;
 import com.it.doubledi.cinemamanager.domain.File;
 import com.it.doubledi.cinemamanager.domain.command.FileCreateCmd;
 import com.it.doubledi.cinemamanager.domain.repository.FileRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.UUID;
 
 @Slf4j
 @Service
 public class FileServiceImpl implements FileService {
 
-    @Value("${application.bucket.name}")
-    private String bucketName;
+    private String bucket = "doubledi-cinema.appspot.com";
 
-    private final AmazonS3 s3client;
     private final FileRepository fileRepository;
-    private final String prefix = "https://cinema-storage.s3.ap-northeast-1.amazonaws.com/";
+    private StorageOptions storageOptions;
+    private final String prefix = "https://firebasestorage.googleapis.com/v0/b/doubledi-cinema.appspot.com/o/";
+    private final String postfix = "?alt=media";
 
     public FileServiceImpl(AmazonS3 s3client, FileRepository fileRepository) {
-        this.s3client = s3client;
         this.fileRepository = fileRepository;
+    }
+
+    @PostConstruct
+    private void initalizeFirebase() throws IOException {
+        InputStream data = Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("firebase/doubledi-cinema-firebase-adminsdk-njo8w-c57accce4a.json"));
+        this.storageOptions = StorageOptions.newBuilder()
+                .setCredentials(GoogleCredentials.fromStream(data))
+                .build();
     }
 
     @Override
@@ -41,13 +53,23 @@ public class FileServiceImpl implements FileService {
         if (Objects.isNull(fileObj)) {
             return null;
         }
-        String url = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-        s3client.putObject(new PutObjectRequest(this.bucketName, url, fileObj).withCannedAcl(CannedAccessControlList.PublicRead));
+        java.io.File convertFile = this.convertMultiPartFileToFile(file);
+        Path filePath = convertFile.toPath();
+        Storage storage = storageOptions.getService();
+        String filename = UUID.randomUUID() + file.getOriginalFilename();
+        BlobId blobId = BlobId.of(bucket, filename);
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(file.getContentType()).build();
+        try {
+            storage.create(blobInfo, Files.readAllBytes(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         FileCreateCmd cmd = FileCreateCmd.builder()
                 .originalName(file.getOriginalFilename())
                 .type(file.getContentType())
                 .size(file.getSize())
-                .path(prefix + url)
+                .path(prefix + filename + postfix)
                 .build();
 
         File fileDomain = new File(cmd);
