9d89c394c64f7c57212ed7b76c42cc3236bfcf0c
dong.dm <dong.dm@evotek.vn>
2022-12-23 17:02:30 +0700

    st

--- src/main/java/com/it/doubledi/cinemamanager/_common/util/DateUtils.java
+++ src/main/java/com/it/doubledi/cinemamanager/_common/util/DateUtils.java
@@ -6,18 +6,21 @@ import java.util.Date;
 
 public class DateUtils {
 
+    private DateUtils() {
+    }
+
     public static Instant getFirstDayOfYear(Instant time) {
         Date date = Date.from(time);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.set(calendar.get(Calendar.YEAR), 1, 1, 0, 0, 0);
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.JANUARY, 1, 0, 0, 0);
         return calendar.toInstant();
     }
 
     public static Instant getLastDayOfYear(Instant time) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(Date.from(time));
-        calendar.set(calendar.get(Calendar.YEAR), 12, 31, 0, 0, 0);
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.DECEMBER, 31, 0, 0, 0);
         return calendar.toInstant();
     }
 }
